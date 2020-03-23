ce9d5f26e42babe342b14a5d077f0112f0da3fa8
aliahmadcse <aliahmadcse@gmail.com>
2020-03-23 18:38:29 +0500

    Adds phase 1 of inventory_update algorithm

---
+++ Algorithms/inventory_update.py
@@ -0,0 +1,30 @@
+def updateInventory(arr1, arr2):
+    #  All inventory must be accounted for or you're fired!
+    result = []
+    for item in arr1:
+        for ele in arr2:
+            if item[1] in ele[1]:
+                result.append([item[0]+ele[0], item[1]])
+    return result
+
+
+#  Example inventory lists
+curInv = [
+    [21, "Bowling Ball"],
+    [2, "Dirty Sock"],
+    [1, "Hair Pin"],
+    [5, "Microphone"]
+]
+
+newInv = [
+    [2, "Hair Pin"],
+    [3, "Half-Eaten Apple"],
+    [67, "Bowling Ball"],
+    [7, "Toothpaste"]
+]
+
+# should return
+# [[88, "Bowling Ball"], [2, "Dirty Sock"],
+#  [3, "Hair Pin"], [3, "Half-Eaten Apple"],
+#  [5, "Microphone"], [7, "Toothpaste"]]
+print(updateInventory(curInv, newInv))
